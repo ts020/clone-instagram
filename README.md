993281857be01489db24a2edfb6e34f47c7a4775
takanori sugawara <takanori.sugawara@gmail.com>
2018-12-10 19:38:04 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# clone-instagram
-Created with CodeSandbox
---
+++ src/components/organisms/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export function Header() {
+  return (
+    <div class="b5itu">
+      <div class="mXkkY HOQT4">
+        <button class="mTGkH">Camera</button>
+      </div>
+      <h1 class="K3Sf1">Logo</h1>
+      <div class="mXkkY KDuQp">
+        <button class="mTGkH">add User</button>
+      </div>
+    </div>
+  );
+}
+//
